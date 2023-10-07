37ca93275b002de96aae70c00df1ddefd64e0a23
Daniel Araújo <dannielaraujooficial@gmail.com>
2023-10-07 15:03:12 -0300

    add method 'open_yotube'

--- services/bot.py
+++ services/bot.py
@@ -8,7 +8,12 @@ class bot():
     def __init__(self):
         service = webdriver.chrome.service.Service(r'C:\Users\danni\OneDrive\Documentos\GitHub\automacao_youtube\chromedriver.exe')
         self.drive = webdriver.Chrome(service=service)
+    
+    def open_youtube(self):
+        drive = self.drive
+        drive.get('https://www.youtube.com/channel/UC64-SF8ykrOum7YVGb7-F5g')
 
 
 if __name__ == '__main__':
     bot = bot()
+    bot.open_youtube()
